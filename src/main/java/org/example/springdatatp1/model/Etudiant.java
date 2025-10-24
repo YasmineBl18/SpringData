ea92a21e33401164a97e2323dcb54370424f5ccb
Yasmine Bourijal <yasbourijal@gmail.com>
2025-10-25 00:58:28 +0100

    first commit

---
+++ src/main/java/org/example/springdatatp1/model/Etudiant.java
@@ -0,0 +1,26 @@
+package org.example.springdatatp1.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.example.springdatatp1.enums.Genre;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name="etudiants")
+public class Etudiant {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Long id;
+    @Column(name="nom_etudiant", nullable=false)
+    String nom;
+    @Column(name="prenom_etudiant")
+    String prenom;
+    @Enumerated(EnumType.STRING)
+    Genre genre;
+}
